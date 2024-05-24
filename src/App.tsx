c31cc38db7d167a7908336ad8884fe84af132b02
VitaliiP-DevBrother <vitalii.p@devbrother.com>
2024-05-24 18:17:26 +0300

    feat: vite test

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <button onClick={async () => {
+          console.log(navigator.canShare())
+          try {
+            await navigator.share({ title: 'Test', text: 'test text |', url: 'https://vitejs.dev/guide/' });
+          } catch (e) {
+            console.log('Failed to share', e);
+          }
+        }}>Share test</button>
+      </div>
+    </>
+  )
+}
+
+export default App
