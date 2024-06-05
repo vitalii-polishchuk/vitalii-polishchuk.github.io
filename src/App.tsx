dfbf7f98aa4413cf7a23296cfca7ab4c0e82378b
VitaliiP-DevBrother <vitalii.p@devbrother.com>
2024-06-05 18:46:10 +0300

    test 3

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
         <button onClick={async () => {
           console.log(navigator.canShare())
           try {
-            await navigator.share({ title: 'Test', text: 'Hey, I think you would like this! Register today and receive rewards: http://localhost:8080/login?ref-code=8c28fa32-e005-4241-8286-a41ab22b8d8a' });
+            await navigator.share({ title: 'Test', text: 'Hey, I think you would like this! Register today and receive rewards: ', url: 'http://localhost:8080/login?ref-code=8c28fa32-e005-4241-8286-a41ab22b8d8a' });
           } catch (e) {
             console.log('Failed to share', e);
           }
